2361b0f374299dc55248c652495391b4a6efb2fa
Ricardo Canteros <richardlihue@gmail.com>
2022-06-18 16:45:46 -0300

    modificacion bakend

--- src/main/java/com/porfolio/richard/RichardApplication.java
+++ src/main/java/com/porfolio/richard/RichardApplication.java
@@ -14,11 +14,19 @@ import java.util.Arrays;
 @SpringBootApplication
 public class RichardApplication {
 
-        public static void main(String[] args) {
-                SpringApplication.run(RichardApplication.class, args);                                                                               }
-@Bean                                                                                                                                                public CorsFilter corsFilter() {                                                                                                                     CorsConfiguration corsConfiguration = new CorsConfiguration();                                                                               corsConfiguration.setAllowCredentials(true);                                                                                                 corsConfiguration.setAllowedOrigins(Arrays.asList("https://richard2022.herokuapp.com/"));                                                    corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",                                                   "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-                                "Access-Control-Request-Method", "Access-Control-Request-Headers"));                                                         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",                                                       "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                          corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));                                               UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-                urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        public static void main(String[] args) { SpringApplication.run(RichardApplication.class, args);                                                                               }
+@Bean
+public CorsFilter corsFilter() {   
+	CorsConfiguration corsConfiguration = new CorsConfiguration();
+	corsConfiguration.setAllowCredentials(true);
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontedargentinaprogramrichard.herokuapp.com/"));
+	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type","Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                               "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization","Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));   
+	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));  
+	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
                 return new CorsFilter(urlBasedCorsConfigurationSource);
         }
 
