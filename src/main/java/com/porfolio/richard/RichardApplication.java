4fc3734d85705c6a669e1c54d0eda4750fda5fd9
Ricardo Canteros <richardlihue@gmail.com>
2022-06-18 17:09:57 -0300

    cambio URL frontend

--- src/main/java/com/porfolio/richard/RichardApplication.java
+++ src/main/java/com/porfolio/richard/RichardApplication.java
@@ -19,7 +19,7 @@ public class RichardApplication {
 public CorsFilter corsFilter() {   
 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	corsConfiguration.setAllowCredentials(true);
-	corsConfiguration.setAllowedOrigins(Arrays.asList("https://porfolioap1.web.app/porfolio"));
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://porfolioap1.web.app/"));
 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type","Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization","Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));   
