8075755787d5e4c136dc2615a9126332be707a3f
ricardo197578 <richardlihue@gmail.com>
2022-07-10 15:47:06 -0300

    cambio
    Signed-off-by: ricardo197578 <richardlihue@gmail.com>

--- src/main/java/com/porfolio/richard/Controller/PersonaController.java
+++ src/main/java/com/porfolio/richard/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/porfolio")
-@CrossOrigin(origins= {"*"})
+//@CrossOrigin(origins= {"*"})
 
 public class PersonaController{
 	private final PersonaService personaService;
